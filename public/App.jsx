2d6c1e3818a77941129be110274d23687a832cc9
Tushar Upadhyay <tusharup@gmail.com>
2022-05-11 09:46:14 -0400

    Week-1: Separate JSX code

---
+++ public/App.jsx
@@ -0,0 +1,12 @@
+    //option - 1
+        /*const element = React.createElement('div', {title: 'Outer div'},
+            React.createElement('h1', null, 'Hello World!')
+        );
+        */
+    //option - 2
+    const element = (
+        <div title="Outer div">
+            <h1> Hello World! </h1>
+        </div>    
+    );
+    ReactDOM.render(element, document.getElementById('contents'));
